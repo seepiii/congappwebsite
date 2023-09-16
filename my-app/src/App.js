c181a8bd2d100867f350043d2e1774d63a250c77
seepiii <93924225+seepiii@users.noreply.github.com>
2023-09-16 01:55:42 -0500

    Co-authored-by: IcedTea139 <IcedTea139@users.noreply.github.com>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-import {db} from './firebase.js';
+import { db } from './firebase.js';
 import {
   collection,
   getDocs,
   addDoc,
-  updateDoc,
   deleteDoc,
   doc,
-} from "firebase/firestore";
+} from 'firebase/firestore';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 function App() {
-  const [newWorking, setNewWorking] = useState("");
-  const [NewQuality, setNewQuality] = useState("");
-  const [newTemp, setNewTemp] = useState("");
-  const [newBottle, setNewBottle] = useState("");
-
+  const [newWorking, setNewWorking] = useState('');
+  const [newPortaPotty, setNewPortaPotty] = useState('');
+  const [NewQuality, setNewQuality] = useState('');
+  const [newTemp, setNewTemp] = useState('');
+  const [newBottle, setNewBottle] = useState('');
+  const [showPopup1, setShowPopup1] = useState(false); // State for the first popup
+  const [showPopup2, setShowPopup2] = useState(false); // State for the second popup
 
   const [users, setUsers] = useState([]);
-  const fountainCollectionRef = collection(db, "WaterFountain");
+  const fountainCollectionRef = collection(db, 'WaterFountain');
 
   const createUser = async () => {
-    await addDoc(fountainCollectionRef, { working: newWorking, quality: NewQuality, temp: newTemp, bottle: newBottle });
+    await addDoc(fountainCollectionRef, {
+      working: newWorking,
+      quality: NewQuality,
+      temp: newTemp,
+      bottle: newBottle,
+    });
+    setShowPopup1(false); // Close the first popup after creating a new entry
+    setShowPopup2(false); // Close the second popup after creating a new entry
   };
-
-/*
-    const updateUser = async (id, whatever field) => {
-    const userDoc = doc(db, "WaterFountain", id);
-    const newFields = { whatever field: new field value };
-    await updateDoc(userDoc, newFields);
-  };
-*/
 
   const deleteUser = async (id) => {
-    const userDoc = doc(db, "WaterFountain", id);
+    const userDoc = doc(db, 'WaterFountain', id);
     await deleteDoc(userDoc);
   };
 
@@ -48,42 +50,140 @@ function App() {
   }, []);
 
   return (
-    
     <div className="App">
-      <input
-        placeholder="working..."
-        onChange={(event) => {
-          setNewWorking(event.target.value);
-        }}
-      />
-      <input
-        placeholder="quality..."
-        onChange={(event) => {
-          setNewQuality(event.target.value);
-        }}
-      />
-      <input
-        placeholder="temp..."
-        onChange={(event) => {
-          setNewTemp(event.target.value);
-        }}
-      />
-      <input
-        placeholder="bottle filler..."
-        onChange={(event) => {
-          setNewBottle(event.target.value);
-        }}
-      />
+      {/* Button to open the first popup */}
+      <button onClick={() => setShowPopup1(true)}>Enter Water Fountain</button>
 
-      <button onClick={createUser}> Add Water Fountain</button>
+      {/* First Popup */}
+      <Popup open={showPopup1} onClose={() => setShowPopup1(false)}>
+        <div className="popup-content">
+          {/* Dropdown menu for "working" */}
+          <select
+            onChange={(event) => {
+              setNewWorking(event.target.value);
+            }}
+            value={newWorking}
+          >
+            <option value="">Select working...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          {/* Dropdown menu for "quality" */}
+          <select
+            onChange={(event) => {
+              setNewQuality(event.target.value);
+            }}
+            value={NewQuality}
+          >
+            <option value="">Select quality...</option>
+            <option value="Red">Red</option>
+            <option value="Yellow">Yellow</option>
+            <option value="Green">Green</option>
+          </select>
+
+          {/* Input field for "temp" */}
+          <input
+            placeholder="temp..."
+            onChange={(event) => {
+              setNewTemp(event.target.value);
+            }}
+          />
+
+          {/* Dropdown menu for "bottle filler" */}
+          <select
+            onChange={(event) => {
+              setNewBottle(event.target.value);
+            }}
+            value={newBottle}
+          >
+            <option value="">Select bottle filler...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <button onClick={createUser}>Add Water Fountain</button>
+        </div>
+      </Popup>
+
+      {/* Button to open the second popup */}
+      <button onClick={() => setShowPopup2(true)}>Enter Bathroom</button>
+
+      {/* Second Popup */}
+      <Popup open={showPopup2} onClose={() => setShowPopup2(false)}>
+        <div className="popup-content">
+          {/* Dropdown menu for "working" */}
+          <select
+            onChange={(event) => {
+              setNewWorking(event.target.value);
+            }}
+            value={newWorking}
+          >
+            <option value="">Select working...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+          {/* put the code here
+          {/* Portapotty*/}
+          <select
+            onChange={(event) => {
+              setNewPortaPotty(event.target.value);
+            }}
+            value={newPortaPotty}
+          ></select>
+            <option value="">Is it a PortaPotty?</option>
+            <select
+            onChange={(event) => {
+              setNewPortaPotty(event.target.value);
+            }}
+            value={newPortaPotty}
+          ></select>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+
+
+          
+          
+          
+          
+          
+          {/* Dropdown menu for "quality" */}
+          <select
+            onChange={(event) => {
+              setNewQuality(event.target.value);
+            }}
+            value={NewQuality}
+          >
+            <option value="">Select quality...</option>
+            <option value="Red">Red</option>
+            <option value="Yellow">Yellow</option>
+            <option value="Green">Green</option>
+          </select>
+
+          {/* Dropdown menu for "bottle filler" */}
+          <select
+            onChange={(event) => {
+              setNewBottle(event.target.value);
+            }}
+            value={newBottle}
+          >
+            <option value="">Select bottle filler...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <button onClick={createUser}>Add Water Fountain</button>
+        </div>
+      </Popup>
+
       {users.map((user) => {
         return (
-          <div>
-            {" "}
+          <div key={user.id}>
             <h1>working: {user.working}</h1>
             <h1>quality: {user.quality}</h1>
-            <h1>quality: {user.temp}</h1>
-
+            <h1>temp: {user.temp}</h1>
+            <h1>bottle filler: {user.bottle}</h1>
+            <button onClick={() => deleteUser(user.id)}>Delete</button>
           </div>
         );
       })}
@@ -92,53 +192,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-function App() {
-
-  const [users, setUsers] = useState([]);
-  const fountainCollectionRef = collection(db, "users");
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(fountainCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getUsers();
-  }, []);
-
-
-  return (
-    
-    <form className="add">
-      <label htmlFor="bottle">Bottle:</label>
-      <input type="checkbox" name="bottle" />
-      <label htmlFor="filter">Filter:</label>
-      <input type="text" name="filter" />
-      <label htmlFor="temp">Temp:</label>
-      <input type="text" name="temp" />
-      <label htmlFor="working">Working:</label>
-      <input type="checkbox" name="working" />
-    
-      <button>add a new water fountain</button>
-    </form>
-    
-   <div className = "App">
-    {users.map((user) => {
-      <text> dawdaw </text>
-
-      return (
-        <div>
-          {" "}
-          <h1>quality: {user.quality}</h1>
-          <h1>temp: {user.temp}</h1>
-          </div>
-      );
-      })}
-    </div>
-  );
-}
-
-export default App;
-*/
