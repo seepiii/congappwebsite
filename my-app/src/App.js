f63b71d8af16f8c062fdc4cf48b40cbf6a627a71
seepiii <93924225+seepiii@users.noreply.github.com>
2023-09-18 23:44:27 -0500

    i love victor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,15 +14,25 @@ import 'reactjs-popup/dist/index.css';
 
 function App() {
   const [newWorking, setNewWorking] = useState('');
-  const [newPortaPotty, setNewPortaPotty] = useState('');
   const [NewQuality, setNewQuality] = useState('');
   const [newTemp, setNewTemp] = useState('');
   const [newBottle, setNewBottle] = useState('');
-  const [showPopup1, setShowPopup1] = useState(false); // State for the first popup
-  const [showPopup2, setShowPopup2] = useState(false); // State for the second popup
+  const [showPopup1, setShowPopup1] = useState(false);
+
+  const [newPortapotty, setNewPortapotty] = useState('');
+  const [newStandalone, setNewStandalone] = useState('');
+  const [newCleanliness, setNewCleanliness] = useState('');
+  const [newToiletPaper, setNewToiletPaper] = useState('');
+  const [newHandicapAccessible, setNewHandicapAccessible] = useState('');
+  const [newUnisex, setNewUnisex] = useState('');
+  const [newDiaperChangingStation, setNewDiaperChangingStation] = useState('');
+  const [newSoap, setNewSoap] = useState('');
+  const [showPopup2, setShowPopup2] = useState(false);
 
   const [users, setUsers] = useState([]);
+  const [entries2, setEntries2] = useState([]); // For the second set of entries
   const fountainCollectionRef = collection(db, 'WaterFountain');
+  const entries2CollectionRef = collection(db, 'Entries2');
 
   const createUser = async () => {
     await addDoc(fountainCollectionRef, {
@@ -31,8 +41,21 @@ function App() {
       temp: newTemp,
       bottle: newBottle,
     });
-    setShowPopup1(false); // Close the first popup after creating a new entry
-    setShowPopup2(false); // Close the second popup after creating a new entry
+    setShowPopup1(false);
+  };
+
+  const createUser2 = async () => {
+    await addDoc(entries2CollectionRef, {
+      portapotty: newPortapotty,
+      standalone: newStandalone,
+      cleanliness: newCleanliness,
+      toiletPaper: newToiletPaper,
+      handicapAccessible: newHandicapAccessible,
+      unisex: newUnisex,
+      diaperChangingStation: newDiaperChangingStation,
+      soap: newSoap,
+    });
+    setShowPopup2(false);
   };
 
   const deleteUser = async (id) => {
@@ -47,17 +70,23 @@ function App() {
     };
 
     getUsers();
-  }, []);
+  }, [fountainCollectionRef]);
+
+  useEffect(() => {
+    const getEntries2 = async () => {
+      const data = await getDocs(entries2CollectionRef);
+      setEntries2(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getEntries2();
+  }, [entries2CollectionRef]);
 
   return (
     <div className="App">
-      {/* Button to open the first popup */}
       <button onClick={() => setShowPopup1(true)}>Enter Water Fountain</button>
 
-      {/* First Popup */}
       <Popup open={showPopup1} onClose={() => setShowPopup1(false)}>
         <div className="popup-content">
-          {/* Dropdown menu for "working" */}
           <select
             onChange={(event) => {
               setNewWorking(event.target.value);
@@ -69,7 +98,6 @@ function App() {
             <option value="No">No</option>
           </select>
 
-          {/* Dropdown menu for "quality" */}
           <select
             onChange={(event) => {
               setNewQuality(event.target.value);
@@ -82,7 +110,6 @@ function App() {
             <option value="Green">Green</option>
           </select>
 
-          {/* Input field for "temp" */}
           <input
             placeholder="temp..."
             onChange={(event) => {
@@ -90,7 +117,6 @@ function App() {
             }}
           />
 
-          {/* Dropdown menu for "bottle filler" */}
           <select
             onChange={(event) => {
               setNewBottle(event.target.value);
@@ -106,73 +132,100 @@ function App() {
         </div>
       </Popup>
 
-      {/* Button to open the second popup */}
-      <button onClick={() => setShowPopup2(true)}>Enter Bathroom</button>
+      <button onClick={() => setShowPopup2(true)}>Enter Restroom</button>
 
-      {/* Second Popup */}
       <Popup open={showPopup2} onClose={() => setShowPopup2(false)}>
         <div className="popup-content">
-          {/* Dropdown menu for "working" */}
           <select
             onChange={(event) => {
-              setNewWorking(event.target.value);
+              setNewPortapotty(event.target.value);
             }}
-            value={newWorking}
+            value={newPortapotty}
           >
-            <option value="">Select working...</option>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-          {/* put the code here
-          {/* Portapotty*/}
-          <select
-            onChange={(event) => {
-              setNewPortaPotty(event.target.value);
-            }}
-            value={newPortaPotty}
-          ></select>
-            <option value="">Is it a PortaPotty?</option>
-            <select
-            onChange={(event) => {
-              setNewPortaPotty(event.target.value);
-            }}
-            value={newPortaPotty}
-          ></select>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-
-
-          
-          
-          
-          
-          
-          {/* Dropdown menu for "quality" */}
-          <select
-            onChange={(event) => {
-              setNewQuality(event.target.value);
-            }}
-            value={NewQuality}
-          >
-            <option value="">Select quality...</option>
-            <option value="Red">Red</option>
-            <option value="Yellow">Yellow</option>
-            <option value="Green">Green</option>
-          </select>
-
-          {/* Dropdown menu for "bottle filler" */}
-          <select
-            onChange={(event) => {
-              setNewBottle(event.target.value);
-            }}
-            value={newBottle}
-          >
-            <option value="">Select bottle filler...</option>
+            <option value="">Portapotty?...</option>
             <option value="Yes">Yes</option>
             <option value="No">No</option>
           </select>
 
-          <button onClick={createUser}>Add Water Fountain</button>
+          <select
+            onChange={(event) => {
+              setNewStandalone(event.target.value);
+            }}
+            value={newStandalone}
+          >
+            <option value="">Standalone?...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <select
+            onChange={(event) => {
+              setNewCleanliness(event.target.value);
+            }}
+            value={newCleanliness}
+          >
+            <option value="">Level of cleanliness?...</option>
+            <option value="Disgusting">Disgusting</option>
+            <option value="Serviceable">Serviceable</option>
+            <option value="Clean">Clean</option>
+          </select>
+
+          <select
+            onChange={(event) => {
+              setNewToiletPaper(event.target.value);
+            }}
+            value={newToiletPaper}
+          >
+            <option value="">Toilet paper left?...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <select
+            onChange={(event) => {
+              setNewHandicapAccessible(event.target.value);
+            }}
+            value={newHandicapAccessible}
+          >
+            <option value="">Handicap accessible?...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <select
+            onChange={(event) => {
+              setNewUnisex(event.target.value);
+            }}
+            value={newUnisex}
+          >
+            <option value="">Unisex?...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <select
+            onChange={(event) => {
+              setNewDiaperChangingStation(event.target.value);
+            }}
+            value={newDiaperChangingStation}
+          >
+            <option value="">Diaper changing station?...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <select
+            onChange={(event) => {
+              setNewSoap(event.target.value);
+            }}
+            value={newSoap}
+          >
+            <option value="">Soap?...</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+
+          <button onClick={createUser2}>Add Entry2</button>
         </div>
       </Popup>
 
@@ -184,6 +237,21 @@ function App() {
             <h1>temp: {user.temp}</h1>
             <h1>bottle filler: {user.bottle}</h1>
             <button onClick={() => deleteUser(user.id)}>Delete</button>
+          </div>
+        );
+      })}
+
+      {entries2.map((entry2) => {
+        return (
+          <div key={entry2.id}>
+            <h1>Portapotty: {entry2.portapotty}</h1>
+            <h1>Standalone: {entry2.standalone}</h1>
+            <h1>Cleanliness: {entry2.cleanliness}</h1>
+            <h1>Toilet paper left: {entry2.toiletPaper}</h1>
+            <h1>Handicap accessible: {entry2.handicapAccessible}</h1>
+            <h1>Unisex: {entry2.unisex}</h1>
+            <h1>Diaper changing station: {entry2.diaperChangingStation}</h1>
+            <h1>Soap: {entry2.soap}</h1>
           </div>
         );
       })}
