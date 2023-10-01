52addb63719834a29113d909e91652e5afa602bc
chote <chotepong.tangton@gmail.c.om>
2023-10-01 16:08:54 -0500

    w

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDr-ND-CzVqJlpC2gPYHCMDJXK553iLt7Q",
-  authDomain: "waterworks-e2c58.firebaseapp.com",
-  projectId: "waterworks-e2c58",
-  storageBucket: "waterworks-e2c58.appspot.com",
-  messagingSenderId: "617069228471",
-  appId: "1:617069228471:web:042937743a3575b705f832",
-  measurementId: "G-017XR6R6RN"
+  apiKey: "AIzaSyAHWCIxx5ZeFl3L8Qn-WpC4YrqVjWi-g2I",
+  authDomain: "waterw-f6f87.firebaseapp.com",
+  projectId: "waterw-f6f87",
+  storageBucket: "waterw-f6f87.appspot.com",
+  messagingSenderId: "578750789816",
+  appId: "1:578750789816:web:2bc459b0161b53191d5c4d",
+  measurementId: "G-PB5DZ390NR"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
